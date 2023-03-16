d802706cd08bc58c00e015add48cc71e22484696
iqbalbintang <bint4ng.pamungkas@gmail.com>
2023-03-16 12:23:16 +0700

    fix/navbar

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -37,7 +37,34 @@
 <body>
 
   <!-- ======= Header ======= -->
-  @include('layouts.body.header')
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+  
+      <h1 class="logo me-auto"><a href="{{ route('home') }}"><span>MI</span>X</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+  
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="/" >Home</a></li>
+               <li><a href="/about">About Us</a></li>
+          <li><a href="/detail/service" >Services</a></li>
+          <li><a href="/harga"class="active">Pricing</a></li>
+          <li><a href="">Blog</a></li>
+          {{-- <li><a href="">Contact</a></li> --}}
+  
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+  
+      {{-- <div class="header-social-links d-flex">
+        <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a>
+      </div> --}}
+    </div>
+  </header>
 
   <!-- End Header -->
 
